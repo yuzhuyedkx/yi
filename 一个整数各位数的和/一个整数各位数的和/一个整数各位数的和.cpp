918b9ac50004fa17465019c64db5bb0aebb5f30d
yuzhuyedkx <17340194386@163.com>
2025-04-01 08:29:35 +0800

    Add files via upload

---
+++ 一个整数各位数的和/一个整数各位数的和/一个整数各位数的和.cpp
@@ -0,0 +1,21 @@
+﻿//定义一个函数计算一个正整数各位数字之和，要求不使用函数返回值和指针返回运算结果。
+
+#include <iostream>
+using namespace std;
+void Sum(int a, int &s);
+int main()
+{
+    int a = 132453;
+    int s = 0;
+    Sum(a, s);
+    cout << a << "各位数的和为：" << s << endl;
+    return 0;
+}
+
+void Sum(int a, int &s) {
+    s = 0;
+    while (a > 0) {
+        s += a % 10;
+        a/=10;
+    }
+}
