716e25a8b49302c9a84f9d3f6e0507329ff7c055
yuzhuyedkx <17340194386@163.com>
2025-04-04 17:29:26 +0800

    Add files via upload

---
+++ 学生信息 函数/学生信息  函数.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//编写函数显示结构体数组的内容。 
+struct Student{
+	int num;
+	char name[20];
+	float score;
+};
+struct Student stu[5]={{10101,"李欣",97},{10102,"张敏",99},
+	{10103,"刘潇",89},{10104,"朱曦",99},{10105,"颜妍",96}}; 
+int main(int argc, char *argv[]) {
+	int i;
+	for(i=0;i<5;i++)
+	printf("%d  %s  %.2f\n",stu[i].num,stu[i].name,stu[i].score);
+	return 0;
+}
+
