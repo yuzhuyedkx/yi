a75932c4bbe155009692d8341f9ec9ba2ed84da0
yuzhuyedkx <17340194386@163.com>
2025-04-06 20:39:18 +0800

    Add files via upload

---
+++ 立方/立方.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b,c;
+	a=2;
+	b=4;
+	c=a*a*a+b*b*b;
+	printf("%d",c); 
+	return 0;
+}
