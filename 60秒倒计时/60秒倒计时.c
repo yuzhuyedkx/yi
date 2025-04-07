45b4d5f8183b341569ee868eb5dad3d3aa40e8ff
yuzhuyedkx <17340194386@163.com>
2025-04-07 21:13:11 +0800

    Add files via upload

---
+++ 60秒倒计时/60秒倒计时.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b;
+	printf("  60√Îµπº∆ ±\n");
+	system("pause");
+	system("cls");
+	a=60;
+	while(0<a)
+	{
+		printf("%d",a);
+		a=a-1;
+		Sleep(1000);
+		system("cls");
+	} 
+	return 0;
+}
