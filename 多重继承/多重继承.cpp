36b074eb9b674b57409fe1865b4623acc0204c87
yuzhuyedkx <17340194386@163.com>
2025-04-23 22:15:34 +0800

    Add files via upload

---
+++ 多重继承/多重继承.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+class Employee {
+protected:
+    string name;
+    int pay;
+public:
+    Employee(string name, int pay) : name(name), pay(pay) { display(); }
+    void display() { cout << "职工姓名：" << name << " , 工资：" << pay << endl; }
+    ~Employee() { ; }
+};
+class Postgraduate {
+protected:
+    string name;
+    int grade;
+public:
+    Postgraduate(string name, int grade) : name(name), grade(grade) { display(); }
+    void display() { cout << "研究生姓名：" << name << " , 成绩：" << grade << endl; }
+    ~Postgraduate() { ; }
+};
+class on_the_jobPostgraduate : public Employee, public Postgraduate {
+protected:
+    string name;
+    int ID;
+public:
+    on_the_jobPostgraduate(int pay, int grade, string name, int ID) :Employee(name, pay), Postgraduate(name, grade), name(name), ID(ID)
+    {
+        display();
+    }
+    void display() { cout << "在职研究生姓名：" << name << " , 学号：" << ID << " , 工资：" << pay << " , 成绩：" << grade << endl; }
+    ~on_the_jobPostgraduate() { ; }
+};
+int main() {
+    on_the_jobPostgraduate on_jP1(5000, 98, "李华", 01);
+    return 0;
+}
