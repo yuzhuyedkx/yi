89fbd2addc8da544169881377fdc65eecd4be327
yuzhuyedkx <17340194386@163.com>
2025-04-28 21:50:49 +0800

    Add files via upload

---
+++ palindromeNumber/2025-04-28.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+void palindromeTree(char a[], int l, int r);
+int main() {
+    char arr[100];
+    printf("请输入一串数字：");
+    scanf_s("%s", arr);
+    int left = 0;
+    int right = sizeof(arr) / sizeof(arr[0]) - 1;
+    palindromeTree(arr, left, right);
+    return 0;
+}
+
+void palindromeTree(char a[], int l, int r) {
+    while (l <= r) {
+        if (a[l] == a[r]) {
+            l++;
+            r--;
+        }
+        else {
+            printf("不是回文数！");
+            break;
+        }
+    }
+    printf("是回文数！");
+}
